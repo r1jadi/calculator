5673239288595397617af8d70a80c2ab65e46843
Rijad Halili <147926874+r1jadi@users.noreply.github.com>
2023-11-23 15:36:45 +0100

    update

---
+++ calculator.js
@@ -0,0 +1,15 @@
+    
+    let rezultati = document.getElementById("result");
+
+
+    function shkruajVleren(vlera){
+        rezultati.textContent += vlera;
+    }
+
+    function fshije(){
+        rezultati.textContent = " ";
+    }
+
+    function kalkulo(){
+        rezultati.parseInt() = vlera;
+    }
